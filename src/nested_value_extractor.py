0f212c0e2a447fbd504e87ef81f54deb1d2381c9
skatromb <1759463+skatromb@users.noreply.github.com>
2023-10-08 17:45:55 +0200

    `from_obj` -> `json_obj` make wps linter compliant

--- src/nested_value_extractor.py
+++ src/nested_value_extractor.py
@@ -1,20 +1,28 @@
 """Extract a nested value from a json-like dictionary object."""
-from collections.abc import Hashable, Sequence
-from typing import Any
+from collections.abc import Sequence
+from typing import Optional, SupportsIndex, TypeGuard, get_args
 
-Keys = Sequence[Hashable]
+Key = SupportsIndex
+# `Optional` stands for `null` values
+JSONValue = Optional[str | int | float | bool]
+NestedJSON = dict | list
+KEY_ERROR_MSG = 'Keys sequence "{keys}" do not exists in object "{json_obj}"'
+
+
+class StopExtractionError(Exception):
+    """Extraction reached the end of nesting, but keys are not over yet."""
 
 
 def extract_nested_value(
-    from_obj: dict,
-    keys: Keys,
+    json_obj: NestedJSON,
+    keys: Sequence[Key],
     safe=True,
-) -> Any:
+) -> JSONValue | NestedJSON:
     """
     Extract a nested value from a json-like dictionary object.
 
     Args:
-        from_obj: The json-like dictionary to extract the nested value from.
+        json_obj: The json-like dictionary to extract the nested value from.
         keys: A sequence of keys to traverse the nested structure.
         safe: When set to True, returns None
             in case of absence the sequence of keys in a nested structure
@@ -24,29 +32,50 @@ def extract_nested_value(
         Any: The nested value, or None if it could not be found.
 
     Raises:
-        KeyError: when `safe_mode` is False
+        KeyError: when `safe` is False
             and the sequence of keys is absent in a nested structure
     """
-    nested_obj: Any = from_obj
+    nested_obj: JSONValue | NestedJSON = json_obj
 
-    if safe:
-        for key in keys:
-            try:
-                nested_obj = nested_obj.get(key)
-            except AttributeError:
-                nested_obj = None
-
-    else:  # safe
-        for key in keys:
-            string = isinstance(nested_obj, str)
-            not_indexable = not hasattr(nested_obj, "__getitem__")
-            if string or not_indexable:
-                raise KeyError(
-                    (
-                        'Keys sequence "{keys}" do not exists ' +
-                        'in object "{from_obj}"'
-                    ).format(keys=keys, from_obj=from_obj),
-                )
-            nested_obj = nested_obj[key]
+    for key in keys:
+        try:
+            nested_obj = _extract_or_stop(nested_obj, key)
+        except StopExtractionError:
+            if safe:
+                return None
+            raise KeyError(
+                KEY_ERROR_MSG.format(keys=keys, json_obj=json_obj),
+            )
 
     return nested_obj
+
+
+def _extract_or_stop(
+    nested_obj: JSONValue | NestedJSON,
+    key: Key,
+) -> JSONValue | NestedJSON:
+    """Extract element from `nested_obj` by `key` index.
+
+    Args:
+        nested_obj: JSON object (nested or final)
+            to get the nested element by indexing
+        key: key for indexing the `nested_obj`
+
+    Returns:
+        Either another nested JSON or final JSON value
+
+    Raises:
+        StopExtractionError: when element is not found
+
+    """
+    if _is_nested(nested_obj):
+        try:
+            return nested_obj[key]
+        except (KeyError, TypeError):
+            raise StopExtractionError()
+
+    raise StopExtractionError()
+
+
+def _is_nested(json_obj: JSONValue | NestedJSON) -> TypeGuard[NestedJSON]:
+    return isinstance(json_obj, get_args(NestedJSON))
